1be895464b8d5342edd14a907c2cde96defa81e5
WingedSeal <firm09719@gmail.com>
2022-01-26 10:55:56 +0700

    Change copy button

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -4,20 +4,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Button } from 'react-bootstrap';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs/';
 
 
 const CodeBlock = (props) => {
     return (
-        <>
+        <div className="code-block">
             <SyntaxHighlighter language={props.language} style={a11yDark} className="rounded-3 mb-1" showLineNumbers startingLineNumber={props.startline}>
                 {props.code}
             </SyntaxHighlighter>
             <CopyToClipboard text={props.code}>
-                <Button className="btn btn-secondary">Copy</Button>
+                <button className="copy-button">&#128190;</button>
             </CopyToClipboard>
-        </>
+
+        </div>
 
     )
 }
