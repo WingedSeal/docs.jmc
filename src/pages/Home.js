43a59045fddb30669a9ce836133947cc4c1d4fb7
WingedSeal <firm09719@gmail.com>
2022-02-13 23:16:52 +0700

    Fix typo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = ({ setNavActive }) => {
             <h1 className="fs-1 text-center text-md-start m-5 mb-2 fw-bolder">Documentation</h1>
             <div className="paragraphs">
                 <p>Welcome! This is the official documentation for JMC. JavaScript-like Minecraft functions.</p>
-                <p>JMc or JavaScript-like Minecraft function is a custom language designed for minecraft datapack which is <span className="fst-italic">inspired</span> by JavaScript. It needs a compiler to compile it down to minecraft datapack.</p>
+                <p>JMC or JavaScript-like Minecraft function is a custom language designed for minecraft datapack which is <span className="fst-italic">inspired</span> by JavaScript. It needs a compiler to compile it down to minecraft datapack.</p>
                 <h2>Why use JMC?</h2>
                 <p>JMC allows you to write minecraft functions in a better language (.jmc) which is more readable and easier to write. For example, you can declare multiple function in a single file and whitespaces no longer matter which means you can split a single command into multiple line.</p>
                 <p>Normal function from .mcfunction file will not work in JMC, the syntax is almost entirely different.</p>
